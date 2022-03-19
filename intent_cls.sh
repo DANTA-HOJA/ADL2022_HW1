bff0568e1fc5619af1f4ae2095f04b3932dc2539
DANTA-HOJA <rime97410000@gmail.com>
2022-03-19 19:47:42 +0800

    add "intent_cls.sh"

---
+++ intent_cls.sh
@@ -0,0 +1,5 @@
+echo -e "Test file <= ${1}\n" # -e 配 \n 可以換行
+echo -e "Predict save to => ${2}\n"
+echo -e "Model = LSTM_best_weight_(h_size768)_(b_size64)_(5_epoch).ckpt @ ./ckpt/intent/"
+
+python3 test_intent.py --test_file ${1} --pred_file ${2} --ckpt_path "./ckpt/intent/LSTM_best_weight_(h_size768)_(b_size64)_(5_epoch).ckpt"
