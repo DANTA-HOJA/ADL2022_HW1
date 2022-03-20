1b254ef04182100628e10a9a743ba882df3637ab
DANTA-HOJA <rime97410000@gmail.com>
2022-03-20 15:36:36 +0800

    create+edit *.sh

---
+++ download.sh
@@ -0,0 +1,6 @@
+# download intent_cls model
+
+wget "https://ntucc365-my.sharepoint.com/:u:/g/personal/r10945018_ntu_edu_tw/EQMgep3Tj51OgLafQPhoVCUBynUbXTI08gmQeXFW7X1eKQ?e=IDO6ee&download=1" -O "./download/intent_cls_8800.ckpt"
+wget "https://ntucc365-my.sharepoint.com/:u:/g/personal/r10945018_ntu_edu_tw/EQqXooqY7jpOt--lq-EF6jkB7sh9ftV-rVdAVUC2zdE8yg?e=zM2mcR&download=1" -O "./download/intent_cls_8946.ckpt"
+
+# download slot_tag model
