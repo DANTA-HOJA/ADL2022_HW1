d8ed4cddc43f887a98a396a27061678b69f996f7
DANTA-HOJA <rime97410000@gmail.com>
2022-03-20 15:53:21 +0800

    add annotation

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+# wget "one drive share link"+"&download=1"
+
 # download intent_cls model
 
 wget "https://ntucc365-my.sharepoint.com/:u:/g/personal/r10945018_ntu_edu_tw/EQMgep3Tj51OgLafQPhoVCUBynUbXTI08gmQeXFW7X1eKQ?e=IDO6ee&download=1" -O "./download/intent_cls_8800.ckpt"
