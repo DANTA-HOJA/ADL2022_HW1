ff17d108c18d4d572fe6857b36458fb325d02147
DANTA-HOJA <rime97410000@gmail.com>
2022-03-20 17:32:37 +0800

    edit

--- intent_cls.sh
+++ intent_cls.sh
@@ -2,4 +2,4 @@ echo -e "Test file <= ${1}\n" # -e 配 \n 可以換行
 echo -e "Predict save to => ${2}\n"
 echo -e "Model = intent_cls_8946.ckpt @ ./download/"
 
-python3 test_intent.py --test_file ${1} --pred_file ${2} --ckpt_path "./download_ckpt/intent_cls_8946.ckpt"
+python3 test_intent.py --test_file ${1} --pred_file ${2} --ckpt_path "./download_ckpt/intent/intent_cls_8946.ckpt"
